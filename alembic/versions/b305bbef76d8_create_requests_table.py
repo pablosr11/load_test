c3dd2bd887416c1d31b358875199b89cce5de901
pablo sanderson <pablo.sanderson@eigentech.com>
2020-10-06 22:03:15 +0100

    Alembic init with first revision and connectin to models, sqlite database and right path

---
+++ alembic/versions/b305bbef76d8_create_requests_table.py
@@ -0,0 +1,24 @@
+"""Create requests table
+
+Revision ID: b305bbef76d8
+Revises: 
+Create Date: 2020-10-06 21:40:04.106028
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'b305bbef76d8'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
