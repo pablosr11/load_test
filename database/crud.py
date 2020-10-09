ca37a8f611278a27d7de86136d9f6a561aaba7a2
pablo sanderson <pablo.sanderson@eigentech.com>
2020-10-09 08:04:08 +0100

    Include crud operations to generate replies, get replies to specific messages etc

--- database/crud.py
+++ database/crud.py
@@ -1,9 +1,14 @@
 from datetime import datetime
+from typing import Optional
 
 from sqlalchemy import desc
 from sqlalchemy.orm import Session
 
 from . import models, schemas
+
+
+def get_request(db: Session, sms_id: int) -> Optional[models.Requests]:
+    return db.query(models.Requests).get(sms_id)
 
 
 def get_requests(db: Session, date: datetime, skip: int = 0, limit: int = 100):
@@ -17,13 +22,37 @@ def get_requests(db: Session, date: datetime, skip: int = 0, limit: int = 100):
     )
 
 
+def get_replies(db: Session, sms_id: int):
+    return db.query(models.Requests).filter(models.Requests.replies_to == sms_id).all()
+
+
+def create_reply(
+    db: Session,
+    original_sms: models.Requests,
+    request: schemas.RequestBase,
+    request_sms: str = None,
+):
+    db_request = models.Requests(
+        origin_ip=request.origin_ip,
+        origin_port=request.origin_port,
+        endpoint=request.endpoint,
+        method=request.method,
+        message=request_sms,
+        replies_to=original_sms.id,
+    )
+    db.add(db_request)
+    db.commit()
+    db.refresh(db_request)
+    return db_request
+
+
 def create_request(db: Session, request: schemas.RequestBase, sms: str = None):
     db_request = models.Requests(
         origin_ip=request.origin_ip,
         origin_port=request.origin_port,
         endpoint=request.endpoint,
         method=request.method,
-        message=sms
+        message=sms,
     )
     db.add(db_request)
     db.commit()
