39749c78860a7bac0663d478e6dc4146ece448ac
pablo sanderson <pablo.sanderson@eigentech.com>
2020-11-05 09:56:35 +0000

    Add basics for redis, with cache policy and max memory

---
+++ server/caches/redis_client.py
@@ -0,0 +1,12 @@
+import os
+
+import redis
+
+redis_cache = redis.StrictRedis(
+    host=os.getenv("REDIS_HOST", "0.0.0.0"),
+    port=os.getenv("REDIS_PORT", "6379"),
+    db=os.getenv("REDIS_CACHE_DB", 0),
+)
+cache_policy = {"lru": "allkeys-lru", "random": "allkeys-random"}
+redis_cache.config_set(name="maxmemory", value="200mb")
+redis_cache.config_set(name="maxmemory-policy", value=cache_policy["random"])
