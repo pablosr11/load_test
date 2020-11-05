301ffd0988e5633e541c3c9c799fffdd7645eace
pablo sanderson <pablo.sanderson@eigentech.com>
2020-11-05 23:54:59 +0000

    Decode redis responses

--- server/caches/redis_client.py
+++ server/caches/redis_client.py
@@ -6,6 +6,7 @@ redis_cache = redis.StrictRedis(
     host=os.getenv("REDIS_HOST", "0.0.0.0"),
     port=os.getenv("REDIS_PORT", "6379"),
     db=os.getenv("REDIS_CACHE_DB", 0),
+    decode_responses=True,
 )
 cache_policy = {"lru": "allkeys-lru", "random": "allkeys-random"}
 redis_cache.config_set(name="maxmemory", value="200mb")
