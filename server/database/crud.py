f6da6f40213063ba7847dbf8060ada427013f00b
pablo sanderson <pablo.sanderson@eigentech.com>
2020-11-07 18:08:40 +0000

    Refactor crud-insert pushing up data serialization

--- server/database/crud.py
+++ server/database/crud.py
@@ -38,32 +38,8 @@ def get_replies(db: Session, sms_id: int) -> List[models.Requests]:
     return db.query(models.Requests).filter(models.Requests.replies_to == sms_id).all()
 
 
-def create_reply(
-    db: Session,
-    original_sms: models.Requests,
-    request: schemas.RequestBase,
-    request_sms: str = None,
-) -> models.Requests:
-    db_request = models.Requests(
-        message=request_sms, replies_to=original_sms.id, **request.dict()
-    )
-    return add_commit_refresh(db=db, request=db_request)
-
-
-def create_request(
-    db: Session, request: schemas.RequestBase, sms: str = None
-) -> models.Requests:
-    db_request = models.Requests(
-        message=sms,
-        **request.dict(),
-    )
-    return add_commit_refresh(db=db, request=db_request)
-
-
-def create_request_from_phone(
-    db: Session, phone_number: str, phone_message: str, request: schemas.RequestBase
-):
-    db_request = models.Requests(
-        message=phone_message, phone=phone_number, **request.dict()
-    )
-    return add_commit_refresh(db=db, request=db_request)
+def create_message(db: Session, message: models.Requests) -> models.Requests:
+    """
+    Given a DB session and a SQLAlchemy model, store it in Database.
+    """
+    return add_commit_refresh(db=db, request=message)
