bc270abfc12a80846d677d3ad1ada20d6f32a70c
pablo sanderson <pablo.sanderson@eigentech.com>
2020-11-23 19:29:01 +0000

    Add tests for all endpoints. Ensure DB gets cleanedup

--- server/tests/test_endpoints.py
+++ server/tests/test_endpoints.py
@@ -1,3 +1,35 @@
+from http import HTTPStatus
+
+MSG_BASE = "/api/sms/"
+
+
 def test_read_main(client):
-    response = client.get("/api/sms")
-    assert response.status_code == 200
+    welcome_page = client.get(MSG_BASE + "hello")
+    assert welcome_page.status_code == HTTPStatus.OK
+
+    # Create one message
+    payload = {"message": "brum"}
+    first_message = client.post(MSG_BASE, json=payload)
+    assert first_message.status_code == HTTPStatus.CREATED
+    assert first_message.json().get("id") == 1
+
+    # Respond to it
+    reply = client.post(MSG_BASE + "1", json=payload)
+    assert reply.status_code == HTTPStatus.CREATED
+    assert reply.json().get("replies_to") == 1
+
+    # Get all messages and check you get both
+    all_messages = client.get(MSG_BASE)
+    assert all_messages.status_code == HTTPStatus.OK
+    assert len(all_messages.json()) == 2
+
+    # Get the original message and check the single reply
+    msg_with_replies = client.get(MSG_BASE + "1")
+    assert msg_with_replies.status_code == HTTPStatus.OK
+    assert len(msg_with_replies.json().get("replies")) == 1
+
+
+def test_database_gets_clean(client):
+    all_messages = client.get(MSG_BASE)
+    assert all_messages.status_code == HTTPStatus.OK
+    assert not all_messages.json()  # empty list
